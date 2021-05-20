03a5cd727cf3f29a0e7ff99cd2b3ceaa0a87b0e6
Thiago Lourin <thiagolourin@MacBook-Pro-de-Thiago.local>
2021-05-20 18:42:36 -0300

    Project creating

---
+++ wtest/wtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wtest
+//
+//  Created by Thiago Lourin on 20/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
